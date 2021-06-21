971662f916e66af7b46be1257cb33c027d358404
turpp <white.tripp@icloud.com>
2021-06-20 22:09:03 -0500

    added components folder and got rid of boiler plate in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>There is going to be a bunch of cool stuff here about me</p>
     </div>
   );
 }
