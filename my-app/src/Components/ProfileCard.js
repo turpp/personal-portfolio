743206e2be9438da4352d6feadb17ba8f7349d62
turpp <white.tripp@icloud.com>
2021-06-27 16:33:17 -0500

    div to show my contact infomrtion and photo

--- my-app/src/Components/ProfileCard.js
+++ my-app/src/Components/ProfileCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProfilePic from '../photos/profilePic.jpeg'
 
 function ProfileCard(){
     return (
-        <div className= "bg-blue-50 ">
-            test
+        <div className= "bg-blue-50 my-auto ml-4 w-2/4 h-full">
+            <img src={ProfilePic} className='transform scale-75 -translate-y-16'/>
         </div>
     )
 }
