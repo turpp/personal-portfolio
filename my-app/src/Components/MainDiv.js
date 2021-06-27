99279a8f855e08c9ba25a229e794de4ea87dbba9
turpp <white.tripp@icloud.com>
2021-06-27 17:37:32 -0500

    adjusted size of main div to make a little smaller

--- my-app/src/Components/MainDiv.js
+++ my-app/src/Components/MainDiv.js
@@ -3,7 +3,7 @@ import ProfileCard from './ProfileCard'
 
 function MainDiv(){
     return (
-        <div className='flex flex-col m-auto w-4/5 content-center h-3/4 bg-white'>
+        <div className='flex flex-col m-auto w-2/3 content-center h-3/4 bg-white'>
             <ProfileCard/>
         </div>
     )
