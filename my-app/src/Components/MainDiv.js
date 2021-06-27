aa52cbad40e83a3d471b2b16c05ff4ae315db514
turpp <white.tripp@icloud.com>
2021-06-27 16:33:04 -0500

    positioned on page centered and added temp background and added profile card div inside

--- my-app/src/Components/MainDiv.js
+++ my-app/src/Components/MainDiv.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProfileCard from './ProfileCard'
 
 function MainDiv(){
     return (
-        <div>
-            sample text
+        <div className='flex flex-col m-auto w-4/5 content-center h-3/4 bg-white'>
+            <ProfileCard/>
         </div>
     )
 }
