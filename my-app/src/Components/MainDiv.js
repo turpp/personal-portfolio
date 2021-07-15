06e3fd4bcac0267a63ad71fe87d1c11ee368f035
turpp <white.tripp@icloud.com>
2021-07-14 20:18:55 -0500

    added navBar component

--- my-app/src/Components/MainDiv.js
+++ my-app/src/Components/MainDiv.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProfileCard from './ProfileCard'
 import AboutMe from './AboutMe'
+import NavBar from './NavBar'
 
 function MainDiv(){
     return (
         <div className='flex flex-row m-auto w-2/3 content-center items-center h-3/4 max-h-mainDiv'>
+            <NavBar/>
             <ProfileCard/>
             <AboutMe/>
         </div>
