52eb7bde053bed4c7adf93dfc0fed7fecc3b5856
turpp <white.tripp@icloud.com>
2021-07-16 15:28:09 -0500

    added state and switch statement to show correct page

--- my-app/src/Components/MainDiv.js
+++ my-app/src/Components/MainDiv.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ProfileCard from './ProfileCard'
 import AboutMe from './AboutMe'
 import NavBar from './NavBar'
+import Resume from './Resume'
 
 function MainDiv(){
+
+    const [showPage, setShowPage] = useState('about')
+
+    function displayPage(){
+    switch (showPage) {
+        case "about":
+            return <AboutMe/>
+        case "resume":
+            return <Resume/>
+        default:
+            break;
+    }
+}
+
     return (
         <div className='flex flex-row m-auto w-3/4 content-center items-center h-5/6 max-h-mainDiv font-newFont'>
             <NavBar/>
             <ProfileCard/>
-            <AboutMe/>
+            {displayPage()}            
         </div>
     )
 }
