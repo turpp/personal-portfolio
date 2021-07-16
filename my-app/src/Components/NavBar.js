95a93038d0d6b1621e81ac695fc42dc9bf3b1e79
turpp <white.tripp@icloud.com>
2021-07-16 14:22:29 -0500

    converted to buttons and centered on nav with full width

--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar(){
     return (
         <div className="bg-white shadow rounded-lg">
-            <div className="grid justify-items-center border-b-2 p-2"> 
+            <button className="grid justify-items-center border-b-2 p-2 w-full"> 
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
@@ -11,10 +11,10 @@ function NavBar(){
             </div>
             <p className='p-2'>About</p>
             
-        </div>
+        </button>
 
         
-            <div className="grid justify-items-center border-b-2 pt-2">
+            <button className="grid justify-items-center border-b-2 pt-2 w-full">
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
@@ -22,9 +22,9 @@ function NavBar(){
                  </div>           
                 <p className='p-2'>Resume</p>
                 
-            </div>
+            </button>
 
-            <div className="grid justify-items-center border-b-2 pt-2">
+            <button className="grid justify-items-center border-b-2 pt-2 w-full">
                 <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
@@ -33,9 +33,9 @@ function NavBar(){
             </div>
             <p className='p-2'>Projects</p>
             
-        </div>
+        </button>
 
-        <div className="grid justify-items-center border-b-2 pt-2">
+        <button className="grid justify-items-center border-b-2 pt-2 w-full">
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
@@ -43,9 +43,9 @@ function NavBar(){
             </div>
             <p className='p-2'>Blog</p>
             
-        </div>
+        </button>
 
-        <div className="grid justify-items-center  pt-2">
+        <button className="grid justify-items-center  pt-2 w-full">
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
@@ -53,7 +53,7 @@ function NavBar(){
             </div>
             <p className='p-2'>Contact</p>
             
-        </div>
+        </button>
 
 
 
