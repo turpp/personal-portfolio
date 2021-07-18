cb949ed0c8458456dda4dd902415f6c0516a2963
turpp <white.tripp@icloud.com>
2021-07-17 23:59:54 -0500

    initial setup for projects page

---
+++ my-app/src/Components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Projects(){
+
+    return(
+        <div className="flex w-full bg-white h-3/4 rounded-sm">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
