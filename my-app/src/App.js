f5b0ce3b046c5824c441444cf79ed0c755571880
turpp <white.tripp@icloud.com>
2021-07-20 15:11:18 -0500

    removed the flex

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import MainDiv from './Components/MainDiv';
 
 function App() {
   return (
-    <div className="App w-screen h-screen bg-gradient-to-r from-green-400 to-blue-500 flex content-center">
+    <div className="App w-screen h-screen bg-gradient-to-r from-green-400 to-blue-500">
       <MainDiv/>
     </div>
   );
