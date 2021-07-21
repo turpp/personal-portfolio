ec18150a276f330f3d90e424982fff184fbbeec5
turpp <white.tripp@icloud.com>
2021-07-21 15:34:03 -0500

    added rest of projects and formatted for mobile

--- my-app/src/Components/Projects.js
+++ my-app/src/Components/Projects.js
@@ -1,22 +1,47 @@
 import React from 'react'
 import WM from '../photos/WM.png'
 import dateSideKick from '../photos/dateSideKick.png'
-
+import myRound from '../photos/myRound.png'
+import TournamentWizard from '../photos/TournamentWizard.png'
+import ecookbook from '../photos/ecookbook.png'
+import movieMadness from '../photos/movieMadness.png'
 export default function Projects(){
 
     return(
-        <div className="flex w-full bg-white h-3/4 rounded-sm">
-            <div className='grid grid-cols-2'>
-                <div className='col-span-2 border-b-4'>
-                    <p className=''>Projects</p>
+        <div className="w-full bg-white rounded-sm p-4">
+            <div className='overflow-auto'>
+            <div className=''>
+                <div className='border-b-2'>
+                    <p className='font-newFont text-2xl font-bold p-2 text-left'>Projects</p>
                 </div>
-                <div className='col-span-1'>
-                    <img className='m-auto w-3/4' src={WM} alt='screen shot of Wealth Management of the Ozarks on Desktop computer'/>
+                <div className='pt-8'>
+                    <img className='m-auto w-full' src={WM} alt='screen shot of Wealth Management of the Ozarks on Desktop computer'/>
+                    <p className=''>Wealth Management of the Ozarks</p>
                 </div>
-                <div className='col-span-1'>
-                    <img className='m-auto w-3/4' src={dateSideKick} alt='Screen shot of Date Sidekick on Desktop and phone'/>
+                <div className='pt-8'>
+                    <img className='m-auto w-full' src={dateSideKick} alt='Screen shot of Date Sidekick on Desktop and phone'/>
+                    <p className=''>Date Sidekick</p>
                 </div>
+                <div className='pt-8'>
+                    <img className='m-auto w-full' src={myRound} alt='Screen shot of Date Sidekick on Desktop and phone'/>
+                    <p className=''>myRound</p>
+                </div>
+                <div className='pt-8'>
+                    <img className='m-auto w-full' src={TournamentWizard} alt='Screen shot of Date Sidekick on Desktop and phone'/>
+                    <p className=''>Tournament Wizard</p>
+                </div>
+                <div className='pt-8'>
+                    <img className='m-auto w-full' src={ecookbook} alt='Screen shot of Date Sidekick on Desktop and phone'/>
+                    <p className=''>Ecookbook</p>
+                </div>
+                <div className='pt-8'>
+                    <img className='m-auto w-full' src={movieMadness} alt='Screen shot of Date Sidekick on Desktop and phone'/>
+                    <p className=''>Movie Madness</p>
+                </div>
+
+
                 
+            </div>
             </div>
         </div>
     )
