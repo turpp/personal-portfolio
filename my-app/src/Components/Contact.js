82dbfa7368f33d3c37ce900c589d74acaef373cf
turpp <white.tripp@icloud.com>
2021-07-21 15:33:45 -0500

    formatted for mobile

--- my-app/src/Components/Contact.js
+++ my-app/src/Components/Contact.js
@@ -16,27 +16,27 @@ export default function Contact(){
 
 
     return(
-        <div className='w-full bg-white h-3/4 rounded-sm'>
+        <div className='w-full bg-white rounded-sm p-4'>
             <div className='m-4 h-3/4'>
                     <p className='font-bold text-center text-4xl'>Let's get in touch!</p>
                     <div className='grid justify-items-center h-full'>
-                    <p className='font-thin text-center text-lg  mb-2'>Please complete the contact form and I'll be in touch.</p>
-                    <form className="contact-form m-auto w-3/4 " onSubmit={sendEmail}>
+                    <p className='font-thin text-center text-xs  mb-4'>Please complete the contact form and I'll be in touch.</p>
+                    <form className="contact-form m-auto w-5/6 " onSubmit={sendEmail}>
                         <div className='grid grid-cols-2 gap-4'>
                         <input type="hidden" name="contact_number" />
                         <div className='col-span-2'>
-                        <input className='border-2 w-full' type="text" name="user_name" placeholder='Name' required/></div>
+                        <input className='w-full shadow-lg border pl-2' type="text" name="user_name" placeholder='Name' required/></div>
                         <div className='col-span-1'>
-                            <input className='border-2 w-full' type="email" name="user_email" placeholder='Email' required/>
+                            <input className='w-full shadow-lg border pl-2' type="email" name="user_email" placeholder='Email' required/>
                         </div>
                         <div className='col-span-1'>
-                            <input className='border-2 w-full' type="phone" name="user_phone" placeholder='Phone'/>
+                            <input className='w-full shadow-lg border pl-2' type="phone" name="user_phone" placeholder='Phone'/>
                         </div>
                         <div className='col-span-2'>
-                        <textarea className='border-2 w-full' name="message" placeholder='Write Your Message'/>
+                        <textarea className='w-full shadow-lg border pl-2' name="message" placeholder='Write Your Message'/>
                         </div>
                         <div className='col-span-2'>
-                        <input className='' type="submit" value="Send" onClick={()=>sendEmail} />
+                        <button className='bg-blue-500 py-2 px-8 rounded-md text-white shadow' type="submit" value="Send" onClick={()=>sendEmail}>Send</button>
                         </div>
                         </div>
                     </form>
