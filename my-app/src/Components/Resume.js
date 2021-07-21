72a53a597ea608023bc4b608353a2f2f754a70d2
turpp <white.tripp@icloud.com>
2021-07-21 11:40:47 -0500

    formatted for mobile first design

--- my-app/src/Components/Resume.js
+++ my-app/src/Components/Resume.js
@@ -4,22 +4,22 @@ import FlatironLogo from '../photos/FlatironLogo.png'
 
 export default function Resume(){
     return(
-        <div className="flex w-full bg-white h-3/4 rounded-sm">
-            <div className='flex-1 overflow-auto'>
+        <div className="w-full bg-white rounded-sm p-4">
+            <div className='overflow-auto'>
                 <div className='border-b-2'>
-                    <p className='text-left font-bold text-2xl p-2'>Resume</p>
+                    <p className='font-newFont text-2xl font-bold p-2 text-left'>Resume</p>
                 </div>
                 <div className="grid grid-cols-1 border-b-2">
-                    <p className='p-4'>An innovative thinker who takes pride in high quality work. My experience of being a business owner ,as a Financial Advisor, gives me the unique skills of leading a team, client relations, problem solving, listening, and a relentless solution focus mindset. I know how to break down problems, find solutions and present these solutions in a simplistic actionable manner. I have a passion for learning and helping others succeed.</p>
+                    <p className='font-newFont  my-1 text-left pl-2'>An innovative thinker who takes pride in high quality work. My experience of being a business owner ,as a Financial Advisor, gives me the unique skills of leading a team, client relations, problem solving, listening, and a relentless solution focus mindset. I know how to break down problems, find solutions and present these solutions in a simplistic actionable manner. I have a passion for learning and helping others succeed.</p>
                 </div>
-                <div className='grid grid-cols-3 border-b-2'>
+                <div className='grid grid-cols-1 border-b-2'>
                     <div className='col-span-1'>
-                        <img src={ATUlogo} alt='Arkansas Tech University Logo' className='w-3/4 m-auto pt-2'/>
+                        <img src={ATUlogo} alt='Arkansas Tech University Logo' className='w-1/2 m-auto mt-4'/>
                     </div>
                     <div className='col-span-2 p-4'>
-                        <p className="font-bold text-xl"><u>Arkansas Tech University</u></p>
-                        <p className='font-semibold text-lg'>Bachelors of Science in Business Administration in Economics and Finance</p>
-                        <p>
+                        <p className="font-bold text-xl text-center"><u>Arkansas Tech University</u></p>
+                        <p className='font-semibold text-sm text-left'>Bachelors of Science in Business Administration in Economics and Finance</p>
+                        <p className='mt-4'>
                             <ul className='text-left'>
                                 <li>- Graduated Summa Cum Laude</li>
                                 <li>- Inducted into Beta Gamma Sigma</li>
@@ -30,12 +30,12 @@ export default function Resume(){
                     </div>
 
                     <div className='col-span-1'>
-                        <img src={FlatironLogo} alt='Arkansas Tech University Logo'/>
+                        <img className='w-1/2 m-auto mt-4' src={FlatironLogo} alt='Arkansas Tech University Logo'/>
                     </div>
                     <div className='col-span-2 p-4'>
-                        <p className='font-bold text-xl'><u>Flatiron Schools</u></p>
-                        <p className='font-semibold text-lg'>Software Engineering Program</p>
-                        <p>
+                        <p className='font-bold text-xl text-center'><u>Flatiron Schools</u></p>
+                        <p className='font-semibold text-sm text-left'>Software Engineering Program</p>
+                        <p className='mt-4'>
                             <ul className='text-left'>
                                 <li>- Full Stack Web Development</li>
                                 <li>- Javascript and React.js</li>
@@ -44,9 +44,9 @@ export default function Resume(){
                         </p> 
                     </div>
                 </div>
-                <div className='grid grid-cols-2 border-b-2 p-4'>
+                <div className='grid grid-cols-1 border-b-2 p-4'>
                     
-                        <div className="">
+                        <div className="mb-8">
                             <p className='text-lg font-semibold'>Technical Skills</p>
                             <ul className='text-left pl-8'>
                                 <li>- Javascript</li>
@@ -78,11 +78,11 @@ export default function Resume(){
                         </div>
                     
                     <div className="p-4">
-                        <p className='font-bold text-2xl'>Work History</p>
+                        <p className='font-bold text-2xl mb-8'>Work History</p>
                         <div>
                             <p className='font-semibold text-xl text-left'>Edward Jones</p>
                             <p className='font-medium text-lg text-right'>Financial Advisor, CRPC®</p>
-                            <ul className='text-left'>
+                            <ul className='text-left mt-4'>
                                 <li>- Lead branch team in daily operations.</li>
                                 <li>- Managed client relations with a book of business of over 300 families.</li>
                                 <li>- Administered solutions to complex problems in a simple actionable way.</li>
