57112a6e547c00ef6a69d6e931621d99dd364f90
turpp <white.tripp@icloud.com>
2021-07-21 11:23:28 -0500

    adjusted logos for my contact links logos

--- my-app/src/Components/ProfileCard.js
+++ my-app/src/Components/ProfileCard.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import ProfilePic from '../photos/profilePic.jpeg'
-import Github from '../photos/GitHub-Mark-32px.png'
-import LinkedIn from '../photos/LI-In-Bug.png'
+import Github from '../photos/GitHub-Mark-120px-plus.png'
+import LinkedIn from '../photos/linkedinMark.png'
 import Dev from '../photos/dev-black.png'
 import NavBar from '../Components/NavBar'
 
@@ -11,18 +11,24 @@ function ProfileCard(props){
 
     return (
         <div className= "bg-blue-50  w-full drop-shadow-xl rounded-md mb-4">
-            <img src={ProfilePic} className='object-cover h-2/3 max-h-96 mx-auto rounded-md'/>
+            <img src={ProfilePic} className='object-cover h-2/3 max-h-96 mx-auto rounded-md pb-4'/>
             <p className="text-5xl">Tripp White</p>
             <p className="text-2xl">Software Engineer</p>
-            <br></br>
-            <div className='flex flex-row justify-between w-1/4 mx-auto'>
-            <a href='https://github.com/turpp'><img src={Github} className='w-4'/> </a>
-            <a href='https://www.linkedin.com/in/thomas-tripp-white'><img src={LinkedIn} className="w-4"/> </a>
-            <a href='https://dev.to/turpp'><img src={Dev} className="w-4"/> </a>
+           
+            <div className='flex flex-row justify-between w-1/2 mx-auto'>
+                <div className="flex-1">
+            <a href='https://github.com/turpp'><img src={Github} className="p-2"/> </a>
+            </div>
+            <div className="flex-1">
+            <a href='https://www.linkedin.com/in/thomas-tripp-white'><img src={LinkedIn} className="p-2"/> </a>
+            </div> 
+            <div className="flex-1">
+            <a href='https://dev.to/turpp'><img src={Dev} className="p-2"/> </a>
+            </div>
 </div>
 <br></br>
             <hr></hr>
-        <div className='flex justify-around'>
+        <div className='flex justify-around h-16 items-center' >
             <div className=''>
             <button className=" ">Download Resume</button>
             </div>
