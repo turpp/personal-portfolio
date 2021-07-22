1127b73a34eca9838d20b965a0617c98f37e9529
turpp <white.tripp@icloud.com>
2021-07-22 18:33:08 -0500

    added eventListner to show modal when clicked on project div

--- my-app/src/Components/Projects.js
+++ my-app/src/Components/Projects.js
@@ -14,27 +14,27 @@ export default function Projects(props){
                 <div className='border-b-2'>
                     <p className='font-newFont text-2xl font-bold p-2 text-left'>Projects</p>
                 </div>
-                <div className='pt-8' onClick={()=>props.setWM(true)}>
+                <div className='pt-8' onClick={()=>props.setModal('wm')}>
                     <img className='m-auto w-full' src={WM} alt='screen shot of Wealth Management of the Ozarks on Desktop computer'/>
                     <p className=''>Wealth Management of the Ozarks</p>
                 </div>
-                <div className='pt-8'>
+                <div className='pt-8' onClick={()=>props.setModal('datesidekick')}>
                     <img className='m-auto w-full' src={dateSideKick} alt='Screen shot of Date Sidekick on Desktop and phone'/>
                     <p className=''>Date Sidekick</p>
                 </div>
-                <div className='pt-8'>
+                <div className='pt-8' onClick={()=>props.setModal('myround')}>
                     <img className='m-auto w-full' src={myRound} alt='Screen shot of Date Sidekick on Desktop and phone'/>
                     <p className=''>myRound</p>
                 </div>
-                <div className='pt-8'>
+                <div className='pt-8' onClick={()=>props.setModal('tournamentwizard')}>
                     <img className='m-auto w-full' src={TournamentWizard} alt='Screen shot of Date Sidekick on Desktop and phone'/>
                     <p className=''>Tournament Wizard</p>
                 </div>
-                <div className='pt-8'>
+                <div className='pt-8' onClick={()=>props.setModal('ecookbook')}>
                     <img className='m-auto w-full' src={ecookbook} alt='Screen shot of Date Sidekick on Desktop and phone'/>
                     <p className=''>Ecookbook</p>
                 </div>
-                <div className='pt-8'>
+                <div className='pt-8' onClick={()=>props.setModal('moviemadness')}>
                     <img className='m-auto w-full' src={movieMadness} alt='Screen shot of Date Sidekick on Desktop and phone'/>
                     <p className=''>Movie Madness</p>
                 </div>
