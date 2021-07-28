e813ddc5f7230ddaa2ef0631240884b94c4ef7d7
turpp <white.tripp@icloud.com>
2021-07-28 14:50:06 -0500

    cleaned up. removed br and hr and position buttons at bottom and added padding to make sure no over lap

--- my-app/src/Components/ProfileCard.js
+++ my-app/src/Components/ProfileCard.js
@@ -10,31 +10,28 @@ function ProfileCard(props){
     
 
     return (
-        <div className= "bg-blue-50  w-full drop-shadow-xl rounded-md mb-4  mx-auto max-w-lg lg:col-span-5 lg:shadow-2xl lg:h-5/6 ">
+        <div className= "bg-blue-50  w-full drop-shadow-xl rounded-md mb-4  mx-auto max-w-lg lg:col-span-5 lg:shadow-2xl lg:h-5/6 relative z-40">
             <img src={ProfilePic} className='object-cover mx-auto rounded-md pb-4 w-full'/>
             <p className="text-5xl">Tripp White</p>
             <p className="text-2xl">Software Engineer</p>
-           
-            <div className='flex flex-row justify-between w-1/2 mx-auto'>
+            <div className='flex flex-row justify-between w-1/2 mx-auto pb-16 lg:pb-0'>
                 <div className="flex-1">
-            <a href='https://github.com/turpp'><img src={Github} className="p-2"/> </a>
+                    <a href='https://github.com/turpp'><img src={Github} className="p-2"/> </a>
+                </div>
+                <div className="flex-1">
+                    <a href='https://www.linkedin.com/in/thomas-tripp-white'><img src={LinkedIn} className="p-2"/> </a>
+                </div> 
+                <div className="flex-1">
+                    <a href='https://dev.to/turpp'><img src={Dev} className="p-2"/> </a>
+                </div>
             </div>
-            <div className="flex-1">
-            <a href='https://www.linkedin.com/in/thomas-tripp-white'><img src={LinkedIn} className="p-2"/> </a>
-            </div> 
-            <div className="flex-1">
-            <a href='https://dev.to/turpp'><img src={Dev} className="p-2"/> </a>
-            </div>
-</div>
-<br></br>
-            <hr></hr>
-        <div className='flex justify-around h-16 items-center' >
-            <div className=''>
-            <button className=" ">Download Resume</button>
-            </div>
-            <div>
-            <button className=' '>Contact Me</button>
-            </div>
+            <div className='flex justify-around h-16 items-center absolute bottom-0 w-full border-t-2' >
+                <div className=''>
+                    <button className=" p-2 rounded-lg bg-blue-800 text-white shadow ">Download Resume</button>
+                </div>
+                <div>
+                    <button className=' p-2 rounded-lg bg-gray-500 text-white shadow '>Contact Me</button>
+                </div>
             </div>
         </div>
     )
