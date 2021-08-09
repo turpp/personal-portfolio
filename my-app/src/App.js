6529faa07d46a724774972d820748ba99a637540
turpp <white.tripp@icloud.com>
2021-08-08 20:56:05 -0500

    added partiles to background

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,104 @@
 import logo from './logo.svg';
 import './App.css';
 import MainDiv from './Components/MainDiv';
+import Particles from 'react-tsparticles';
 
 function App() {
+
+  function particlesInit (main) {
+    console.log(main)
+  }
+
+  function particlesLoaded(container){
+    console.log(container)
+  }
   return (
-    <div className="App">
-      <MainDiv/>
+    <div className="App relative">
+      <Particles
+        className='absolute top-0 left-0 w-screen h-screen'
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#0d47a1",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <MainDiv className=""/>
     </div>
   );
 }
