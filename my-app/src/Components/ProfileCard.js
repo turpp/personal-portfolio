0b1c6d1a6ea95c8c73b1f1379bb23e9dfef79664
turpp <white.tripp@icloud.com>
2021-08-10 17:12:11 -0500

    added scroll for overflow on small screen

--- my-app/src/Components/ProfileCard.js
+++ my-app/src/Components/ProfileCard.js
@@ -10,11 +10,11 @@ function ProfileCard(props){
     
 
     return (
-        <div className= "bg-blue-50  w-full drop-shadow-xl rounded-md mb-4  mx-auto max-w-lg lg:col-span-5 lg:shadow-2xl lg:h-5/6 relative z-40">
-            <img src={ProfilePic} className='object-cover mx-auto rounded-md pb-4 w-full'/>
+        <div className= "bg-blue-50  w-full drop-shadow-xl rounded-md mb-4  mx-auto max-w-lg lg:col-span-5 lg:shadow-2xl lg:h-5/6 relative z-40 overflow-auto overscroll-contain">
+            <img src={ProfilePic} className='object-fill mx-auto rounded-md pb-4 w-full' />
             <p className="text-5xl">Tripp White</p>
             <p className="text-2xl">Software Engineer</p>
-            <div className='flex flex-row justify-between w-1/2 mx-auto pb-16 lg:pb-0'>
+            <div className='flex flex-row justify-between w-1/2 mx-auto pb-16 lg:pb-16'>
                 <div className="flex-1">
                     <a href='https://github.com/turpp'><img src={Github} className="p-2"/> </a>
                 </div>
@@ -25,7 +25,7 @@ function ProfileCard(props){
                     <a href='https://dev.to/turpp'><img src={Dev} className="p-2"/> </a>
                 </div>
             </div>
-            <div className='flex justify-around h-16 items-center absolute bottom-0 w-full border-t-2' >
+            <div className='flex justify-around h-16 items-center w-full border-t-2 object-bottom' >
                 <div className=''>
                     <button className=" p-2 rounded-lg bg-blue-800 text-white shadow ">Download Resume</button>
                 </div>
