8740a2338b4e0044b79c8643e9f5b373855c494a
turpp <white.tripp@icloud.com>
2021-08-11 12:49:50 -0500

    changed width and padding on navbar

--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function NavBar(props){
     return (
-        <div className="bg-white shadow flex w-full sm:flex-col  sm:rounded sm:w-24 z-40 relative">
-            <button className="grid justify-items-center border-b-2 p-2 w-full" onClick={()=>props.setPage('about')}> 
+        <div className="bg-white shadow flex w-full sm:flex-col  sm:rounded sm:w-16 desktop:w-20 z-40 relative">
+            <button className="grid justify-items-center border-b-2 p-1 w-full" onClick={()=>props.setPage('about')}> 
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
             </svg>
             </div>
-            <p className='p-2'>About</p>
+            <p className='p-1'>About</p>
             
         </button>
 
@@ -20,7 +20,7 @@ function NavBar(props){
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
                  </div>           
-                <p className='p-2'>Resume</p>
+                <p className='p-1'>Resume</p>
                 
             </button>
 
@@ -31,7 +31,7 @@ function NavBar(props){
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
             </svg>
             </div>
-            <p className='p-2'>Projects</p>
+            <p className='p-1'>Projects</p>
             
         </button>
 
@@ -41,7 +41,7 @@ function NavBar(props){
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
             </svg>
             </div>
-            <p className='p-2'>Blog</p>
+            <p className='p-1'>Blog</p>
             
         </button>
 
@@ -51,7 +51,7 @@ function NavBar(props){
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
             </svg>
             </div>
-            <p className='p-2'>Contact</p>
+            <p className='p-1'>Contact</p>
             
         </button>
 
