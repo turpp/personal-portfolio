fb8af277edcf82fdebd2a61c2325c74cf311f733
turpp <white.tripp@icloud.com>
2021-08-14 20:03:59 -0500

    adjusted the layout to make it take up more screen and center better

--- my-app/src/Components/MainDiv.js
+++ my-app/src/Components/MainDiv.js
@@ -59,11 +59,11 @@ function displayPage(){
         }
     } else{
         return(
-        <div className='grid grid-cols-2 font-newFont sm:grid-cols-12 sm:pt-8 sm:h-screen  md:max-w-3xl lg:max-w-screen-lg desktop:max-w-screen-xl  m-auto lg:max-h-mainDiv relative'>
-            <div className='col-span-2 sticky top-0 sm:col-start-2 lg:col-start-1 my-auto  lg:place-self-end sm:place-self-end sm:mt-4 sm:-mr-4 desktop:mt-40 lg:mt-28 z-50 ' >
+        <div className='grid grid-cols-2 font-newFont sm:grid-cols-12  sm:h-screen  sm:pt-8 md:max-w-3xl lg:pt-0 lg:max-w-full desktop:max-w-screen-xl  m-auto lg:max-h-mainDiv relative'>
+            <div className='col-span-2 sticky top-0 sm:col-start-2 lg:col-start-0 lg:col-span-1  my-auto lg:place-self-end sm:place-self-end sm:mt-4 sm:-mr-4 desktop:mt-40 lg:mt-28 z-50 ' >
                 <NavBar setPage={setShowPage}/>
             </div>
-            <div className='w-full col-span-2 mb-16 sm:col-span-8 sm:overflow-auto lg:h-5/6 lg:col-span-10 lg:pr-8 lg:m-auto '>
+            <div className='w-full col-span-2  sm:col-span-8 sm:overflow-auto lg:h-full lg:col-span-11  lg: col-start-1 lg:pr-8 lg:m-auto '>
                 <div className='lg:grid lg:grid-cols-12 lg:h-full lg:place-items-center '>
                 <ProfileCard setPage={setShowPage}/>
                 {displayContent()}
